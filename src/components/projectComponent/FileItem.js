36b4a255e5e710a8c9e7775e4121e48373fb9f7e
Mark Wu <markwu35@yahoo.com>
2018-02-22 01:25:57 -0800

    fixed download issue

--- src/components/projectComponent/FileItem.js
+++ src/components/projectComponent/FileItem.js
@@ -4,7 +4,8 @@ import './Projects.css';
 class FileItem extends Component {
   render() {
     return (
-      <a className="inline-block" target="_blank" href={this.props.file.link}>{this.props.file.name}</a>
+      <a className="inline-block" target="_blank" href={this.props.file.link} download={this.props.file.name}>{this.props.file.name}</a>
+
     );
   }
 }
